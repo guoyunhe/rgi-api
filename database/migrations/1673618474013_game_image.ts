fbd5994f452e7faf6c394065896044d2cf4b01b1
鹤仙 <guoyunhe.gyh@alibaba-inc.com>
2023-01-19 18:06:47 +0800

    game image table

---
+++ database/migrations/1673618474013_game_image.ts
@@ -0,0 +1,21 @@
+import BaseSchema from '@ioc:Adonis/Lucid/Schema';
+
+export default class extends BaseSchema {
+  protected tableName = 'game_image';
+
+  public async up() {
+    this.schema.createTable(this.tableName, (table) => {
+      table.increments('id').primary();
+      table.integer('game_id').unsigned().references('games.id');
+      table.integer('image_id').unsigned().references('images.id');
+      table.string('image_type').notNullable();
+      table.unique(['game_id', 'image_id']);
+      table.timestamp('created_at', { useTz: true });
+      table.timestamp('updated_at', { useTz: true });
+    });
+  }
+
+  public async down() {
+    this.schema.dropTable(this.tableName);
+  }
+}
