5cb7e2bf54145b8964685c5a50e96729db19728b
Guo Yunhe <i@guoyunhe.me>
2023-01-25 13:24:26 +0800

    fix score type

--- database/migrations/1673556127149_links.ts
+++ database/migrations/1673556127149_links.ts
@@ -12,7 +12,7 @@ export default class extends BaseSchema {
       table.string('url').notNullable().unique();
       table.string('title').nullable();
       table.text('excerpt').nullable();
-      table.decimal('score', 1).nullable();
+      table.decimal('score', 3, 1).nullable(); // 00.0 to 10.0
 
       table.timestamp('created_at', { useTz: true });
       table.timestamp('updated_at', { useTz: true });
