7727308718ba7325481f4bf922e51e4b3de0422c
Guo Yunhe <i@guoyunhe.me>
2023-01-27 15:49:36 +0800

    update url

--- app/Models/Image.ts
+++ app/Models/Image.ts
@@ -51,8 +51,7 @@ export default class Image extends Model {
   /** Image public url */
   @computed()
   public get url() {
-    // return Drive.getUrl(this.path);
-    return `//${Env.get('HOST')}:${Env.get('PORT')}/storage/${this.path}`;
+    return `${Env.get('APP_URL')}/storage/${this.path}`;
   }
 
   public static async createFromLocalFile(
--- server.ts
+++ server.ts
@@ -11,26 +11,9 @@
 */
 
 import { Ignitor } from '@adonisjs/core/build/standalone';
-import { existsSync, readFileSync } from 'fs';
-import { createServer as createHttpServer } from 'http';
-import { createServer as createHttpsServer } from 'https';
 import 'reflect-metadata';
 import sourceMapSupport from 'source-map-support';
 
 sourceMapSupport.install({ handleUncaughtExceptions: false });
 
-new Ignitor(__dirname).httpServer().start((handle) => {
-  const sslKey = `/etc/letsencrypt/live/${process.env.HOST}/privkey.pem`;
-  const sslCert = `/etc/letsencrypt/live/${process.env.HOST}/fullchain.pem`;
-  if (existsSync(sslKey) && existsSync(sslCert)) {
-    return createHttpsServer(
-      {
-        key: readFileSync(sslKey),
-        cert: readFileSync(sslCert),
-      },
-      handle
-    );
-  } else {
-    return createHttpServer(handle);
-  }
-});
+new Ignitor(__dirname).httpServer().start();
