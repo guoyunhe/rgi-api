385ebd3359dc1aa9aebbe7a59055fd356a00a80c
鹤仙 <guoyunhe.gyh@alibaba-inc.com>
2023-01-28 14:57:59 +0800

    filter thumb

--- database/helpers/importLibretroThumb.ts
+++ database/helpers/importLibretroThumb.ts
@@ -21,56 +21,57 @@ export default async function importLibretroThumb(platform: string, repo: string
   for (let i = 0; i < thumbnailTypes.length; i++) {
     const thumbType = thumbnailTypes[i];
     const thumbTypeRoot = `${dist}/Named_${thumbType}s`;
-    const thumbs = await readdir(thumbTypeRoot, { withFileTypes: true });
+    let thumbs = (await readdir(thumbTypeRoot, { withFileTypes: true }))
+      .filter((thumb) => thumb.isFile() && filterThumbnail(thumb.name))
+      .map((thumb) => thumb.name)
+      .sort((a, b) => a.replace('.png', '').localeCompare(b.replace('.png', '')));
 
     let notMatched = 0;
     let total = 0;
 
     for (let j = 0; j < thumbs.length; j++) {
       const thumb = thumbs[j];
-      if (thumb.isFile() && filterThumbnail(thumb.name)) {
-        total++;
-        const name = thumb.name
-          .substring(0, thumb.name.length - 4)
-          .replaceAll(/\s+/g, ' ')
-          .replaceAll('_', '&');
-        const { mainName } = parseName(name);
-        let game = await Game.query().where({ name: mainName, platform }).first();
-        if (!game) {
-          game = await Game.query()
-            .where({ name: mainName + ' (Disc 1)', platform })
-            .first();
-        }
-        if (game?.mainId) {
-          game = await Game.find(game.mainId);
-        }
-        if (game) {
-          const image = await Image.createFromLocalFile(thumbTypeRoot + '/' + thumb.name, {
-            type: thumbType.toLowerCase() as any,
-          });
-          await game.related('images').save(image);
-          await Activity.create({
-            type: 'system',
-            targetType: 'image',
-            targetId: image.id,
-            action: 'import',
-            data: {
-              source: 'libretro-thumbnails',
-            },
-          });
-          await Activity.create({
-            type: 'system',
-            targetType: 'game',
-            targetId: game.id,
-            action: 'addImage',
-            data: {
-              source: 'libretro-thumbnails',
-            },
-          });
-        } else {
-          notMatched++;
-          console.log(platform, thumbType, '\x1b[31m', 'NotMatched', '\x1b[0m', thumb.name);
-        }
+      total++;
+      const name = thumb
+        .substring(0, thumb.length - 4)
+        .replaceAll(/\s+/g, ' ')
+        .replaceAll('_', '&');
+      const { mainName } = parseName(name);
+      let game = await Game.query().where({ name: mainName, platform }).first();
+      if (!game) {
+        game = await Game.query()
+          .where({ name: mainName + ' (Disc 1)', platform })
+          .first();
+      }
+      if (game?.mainId) {
+        game = await Game.find(game.mainId);
+      }
+      if (game) {
+        const image = await Image.createFromLocalFile(thumbTypeRoot + '/' + thumb, {
+          type: thumbType.toLowerCase() as any,
+        });
+        await game.related('images').save(image);
+        await Activity.create({
+          type: 'system',
+          targetType: 'image',
+          targetId: image.id,
+          action: 'import',
+          data: {
+            source: 'libretro-thumbnails',
+          },
+        });
+        await Activity.create({
+          type: 'system',
+          targetType: 'game',
+          targetId: game.id,
+          action: 'addImage',
+          data: {
+            source: 'libretro-thumbnails',
+          },
+        });
+      } else {
+        notMatched++;
+        console.log(platform, thumbType, '\x1b[31m', 'NotMatched', '\x1b[0m', thumb.name);
       }
     }
     console.log(
